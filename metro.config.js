2a0f4f5ec3c79e7e9b564e2d999c565d1a8a883c
liuchangqi63-maker <liuchangqi63@gmail.com>
2026-01-02 07:31:48 +0800

    Watch nativewind cache in Metro

--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,14 @@
+const path = require("path");
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require("nativewind/metro");
 
 const config = getDefaultConfig(__dirname);
+const cssInteropCache = path.resolve(
+  __dirname,
+  "node_modules/react-native-css-interop/.cache",
+);
+
+config.watchFolders = [...(config.watchFolders ?? []), cssInteropCache];
 
 module.exports = withNativeWind(config, {
   input: "./global.css",
