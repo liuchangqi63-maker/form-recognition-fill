1ef70990efe5933eb88765e7112522fe33119e3c
liuchangqi63-maker <liuchangqi63@gmail.com>
2026-01-02 07:55:39 +0800

    Fix NativeWind web build for Vercel

--- metro.config.js
+++ metro.config.js
@@ -1,24 +1,8 @@
-const path = require("path");
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require("nativewind/metro");
 
 const config = getDefaultConfig(__dirname);
 
-const cssInteropRoot = path.dirname(
-  require.resolve("react-native-css-interop/package.json"),
-);
-const cssInteropCache = path.join(cssInteropRoot, ".cache");
-
-const nativewindConfig = withNativeWind(config, {
+module.exports = withNativeWind(config, {
   input: "./global.css",
-  // Force write CSS to file system instead of virtual modules
-  // This fixes iOS styling issues in development mode
-  forceWriteFileSystem: true,
 });
-
-nativewindConfig.watchFolders = [
-  ...(nativewindConfig.watchFolders ?? []),
-  cssInteropCache,
-];
-
-module.exports = nativewindConfig;
